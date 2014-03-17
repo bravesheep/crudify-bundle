8ff23fa8107c16839b1bdcf6e9e8551b40cf0ad6
Ruben Nijveld <ruben@gewooniets.nl>
2014-03-17 12:50:02 +0100

    Added Query modifier resolver spec

--- spec/Bs/CrudifyBundle/Resolver/QueryModifierResolverSpec.php
+++ spec/Bs/CrudifyBundle/Resolver/QueryModifierResolverSpec.php
@@ -2,13 +2,72 @@
 
 namespace spec\Bs\CrudifyBundle\Resolver;
 
+use Bs\CrudifyBundle\Query\ModifierInterface;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class QueryModifierResolverSpec extends ObjectBehavior
 {
+    function let(ContainerInterface $container)
+    {
+        $this->setContainer($container);
+    }
+
     function it_is_initializable()
     {
         $this->shouldHaveType('Bs\CrudifyBundle\Resolver\QueryModifierResolver');
+    }
+
+    function it_should_resolve_a_service(ContainerInterface $container, ModifierInterface $modifier)
+    {
+        $service = 'this_is_a_service';
+        $container->has($service)->willReturn(true);
+        $container->get($service)->willReturn($modifier);
+
+        $this->resolve($service)->shouldReturn($modifier);
+    }
+
+    function it_should_throw_an_error_for_a_nonexistant_service(ContainerInterface $container)
+    {
+        $service = 'this_service_does_not_exist';
+        $container->has($service)->willReturn(false);
+
+        $this
+            ->shouldThrow('Bs\\CrudifyBundle\\Exception\\QueryModifierNotFoundException')
+            ->duringResolve($service)
+        ;
+    }
+
+    function it_should_resolve_a_class(ContainerInterface $container)
+    {
+        $class = 'Bs\\CrudifyBundle\\Tests\\Fixtures\\Query\\MockModifier';
+        $container->has($class)->willReturn(false);
+
+        $this->resolve($class)->shouldBeAnInstanceOf($class);
+    }
+
+    function it_should_throw_an_error_for_a_nonexistant_class(ContainerInterface $container)
+    {
+        $class = 'Acme\\Nonexistant\\Modifier';
+        $container->has($class)->willReturn(false);
+
+        $this
+            ->shouldThrow('Bs\\CrudifyBundle\\Exception\\QueryModifierNotFoundException')
+            ->duringResolve($class)
+        ;
+    }
+
+    function it_should_return_a_passed_modifier_directly(ModifierInterface $modifier)
+    {
+        $this->resolve($modifier)->shouldReturn($modifier);
+    }
+
+    function it_should_throw_an_error_for_incompatible_types()
+    {
+        $this
+            ->shouldThrow('Bs\\CrudifyBundle\\Exception\\QueryModifierNotFoundException')
+            ->duringResolve(10)
+        ;
     }
 }
---
+++ src/Bs/CrudifyBundle/Tests/Fixtures/Query/MockModifier.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Bs\CrudifyBundle\Tests\Fixtures\Query;
+
+use Bs\CrudifyBundle\Query\AbstractModifier;
+
+class MockModifier extends AbstractModifier
+{
+
+}
