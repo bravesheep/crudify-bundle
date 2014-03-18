b5190cb16294b5c9f6e7ebfb502d0974fc5d13d6
Ruben Nijveld <ruben@gewooniets.nl>
2014-03-18 17:58:41 +0100

    Added ability to create users in feature, updated scenarios

--- features/index.feature
+++ features/index.feature
@@ -3,6 +3,21 @@ Feature: Visiting the index page
   As a user
   I need to be able to view the list of objects
 
+  Background:
+    Given the following users exist:
+      | name  | city        | street        |
+      | Joe   | Idaho Falls | Ultricies Rd. |
+      | John  | Arviat      | Morbi Avenue  |
+      | Julia | Hallaar     | Nec, Street   |
+      | Jane  | Tulita      | Ipsum St.     |
+
   Scenario: Getting redirected to the default mapping
-    Given I am on the homepage
-     Then I should be on "/users"
+    When I am on the homepage
+    Then I should be on "/users"
+
+  Scenario:
+    When I am on "/users"
+    Then I should see "Joe"
+    And I should see "Jane"
+    And I should see "Arviat"
+    And I should see "Nec, Street"
--- src/Bs/CrudifyBundle/Tests/Context/FeatureContext.php
+++ src/Bs/CrudifyBundle/Tests/Context/FeatureContext.php
@@ -2,10 +2,36 @@
 
 namespace Bs\CrudifyBundle\Tests\Context;
 
+use Behat\Gherkin\Node\TableNode;
 use Behat\MinkExtension\Context\MinkContext;
 use Behat\Symfony2Extension\Context\KernelDictionary;
+use Bs\CrudifyBundle\Tests\Fixtures\TestBundle\Entity\Address;
+use Bs\CrudifyBundle\Tests\Fixtures\TestBundle\Entity\User;
+use Doctrine\ORM\EntityManager;
 
 class FeatureContext extends MinkContext
 {
     use KernelDictionary;
+
+    /**
+     * @Given /^the following users exist:$/
+     */
+    public function theFollowingUsersExist(TableNode $table)
+    {
+        /** @var EntityManager $em */
+        $em = $this->getContainer()->get('doctrine')->getManagerForClass('TestBundle:User');
+
+        $hash = $table->getHash();
+        foreach ($hash as $row) {
+            $user = new User();
+            $address = new Address();
+            $address->setCity($row['city']);
+            $address->setStreet($row['street']);
+            $user->setName($row['name']);
+            $user->setAddress($address);
+            $em->persist($address);
+            $em->persist($user);
+        }
+        $em->flush();
+    }
 }
