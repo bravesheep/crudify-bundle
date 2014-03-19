daca7b3c1ec26a3e675e95680cdea74a64fdb1e0
Ruben Nijveld <ruben@gewooniets.nl>
2014-03-19 13:01:41 +0100

    Updated index feature behaviors

--- features/index.feature
+++ features/index.feature
@@ -3,21 +3,32 @@ Feature: Visiting the index page
   As a user
   I need to be able to view the list of objects
 
-  Background:
+  Scenario: Getting redirected to the default mapping
+    When I am on the homepage
+    Then I should be on "/users"
+
+  Scenario: I should be able to see the information in the user table
     Given the following users exist:
       | name  | city        | street        |
       | Joe   | Idaho Falls | Ultricies Rd. |
       | John  | Arviat      | Morbi Avenue  |
       | Julia | Hallaar     | Nec, Street   |
       | Jane  | Tulita      | Ipsum St.     |
-
-  Scenario: Getting redirected to the default mapping
-    When I am on the homepage
-    Then I should be on "/users"
-
-  Scenario:
     When I am on "/users"
-    Then I should see "Joe"
-    And I should see "Jane"
+    Then I should see a grid with 4 rows
+    And I should see "Joe"
     And I should see "Arviat"
     And I should see "Nec, Street"
+
+  Scenario: Pagination should be visible if there are many users
+    Given there are 30 users
+    When I am on "/users"
+    Then there should be pagination on the page
+
+  Scenario: I should be able to visit the second page of results
+    Given there are 30 users
+    When I am on "/users"
+    And I click on the next page button
+    Then I should be on the second users page
+    And I should see a grid with 10 rows
+
--- src/Bs/CrudifyBundle/Tests/Context/FeatureContext.php
+++ src/Bs/CrudifyBundle/Tests/Context/FeatureContext.php
@@ -2,24 +2,41 @@
 
 namespace Bs\CrudifyBundle\Tests\Context;
 
+use Behat\Behat\Exception\PendingException;
 use Behat\Gherkin\Node\TableNode;
+use Behat\Mink\Element\DocumentElement;
 use Behat\MinkExtension\Context\MinkContext;
 use Behat\Symfony2Extension\Context\KernelDictionary;
 use Bs\CrudifyBundle\Tests\Fixtures\TestBundle\Entity\Address;
 use Bs\CrudifyBundle\Tests\Fixtures\TestBundle\Entity\User;
-use Doctrine\ORM\EntityManager;
+use Doctrine\Bundle\DoctrineBundle\Registry;
 
 class FeatureContext extends MinkContext
 {
     use KernelDictionary;
 
     /**
+     * @return DocumentElement
+     */
+    public function getPage()
+    {
+        return $this->getSession()->getPage();
+    }
+
+    /**
+     * @return Registry
+     */
+    public function getDoctrine()
+    {
+        return $this->getContainer()->get('doctrine');
+    }
+
+    /**
      * @Given /^the following users exist:$/
      */
     public function theFollowingUsersExist(TableNode $table)
     {
-        /** @var EntityManager $em */
-        $em = $this->getContainer()->get('doctrine')->getManagerForClass('TestBundle:User');
+        $em = $this->getDoctrine()->getManager();
 
         $hash = $table->getHash();
         foreach ($hash as $row) {
@@ -33,5 +50,59 @@ class FeatureContext extends MinkContext
             $em->persist($user);
         }
         $em->flush();
+    }
+
+    /**
+     * @Given /^there are (\d+) users$/
+     */
+    public function thereAreUsers($count)
+    {
+        $faker = \Faker\Factory::create();
+        $em = $this->getDoctrine()->getManager();
+
+        for ($i = 0; $i < (int) $count; $i += 1) {
+            $user = new User();
+            $address = new Address();
+            $address->setCity($faker->city);
+            $address->setStreet($faker->streetName);
+            $user->setName($faker->firstName);
+            $user->setAddress($address);
+            $em->persist($address);
+            $em->persist($user);
+        }
+        $em->flush();
+    }
+
+    /**
+     * @Then /^I should see a grid with (\d+) rows$/
+     */
+    public function iShouldSeeAGridWithRows($count)
+    {
+        $rows = $this->getPage()->findAll('css', '.crudify-grid > tbody > tr');
+        expect(count($rows))->toBe((int) $count);
+    }
+
+    /**
+     * @Then /^there should be pagination on the page$/
+     */
+    public function thereShouldBePaginationOnThePage()
+    {
+        expect($this->getPage()->find('css', 'ul.pagination'))->notToBe(null);
+    }
+
+    /**
+     * @When /^I click on the next page button$/
+     */
+    public function iClickOnTheNextPageButton()
+    {
+        $this->getPage()->findLink('Next »')->click();
+    }
+
+    /**
+     * @Then /^I should be on the second users page$/
+     */
+    public function iShouldBeOnTheSecondUsersPage()
+    {
+        expect($this->getSession()->getCurrentUrl())->toMatch('/page=2/');
     }
 }
